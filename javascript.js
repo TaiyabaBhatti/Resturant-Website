77a62a6c3d9fa074579158e17893eb383ca66ac1
TaiyabaBhatti <taiyababhattii@gmail.com>
2024-06-06 15:19:16 +0500

    added new sections and loading screen

--- javascript.js
+++ javascript.js
@@ -1,48 +1,60 @@
-// const home_section =document.getElementById("home");
-// const load_page=document.getElementById("load-page");
-// function loaded(){
-// // load_page.style.display="none";
-// home_section.classList.add("loaded");
-// home_section.style.display="block";
-// }
-// setTimeout(loaded,2000);
+
+document.addEventListener("DOMContentLoaded",()=>{
+
+AOS.init();
+
+const home_section =document.getElementById("load-page");
+const load_page=document.getElementById("preLoad");
+function loaded(){
+
+load_page.classList.add("loaded");
+// load_page.style.display="none";
+home_section.style.display="block";
+
+}
+ setTimeout(loaded,3000);
+
+
+
+
 
 // Menu effect
-AOS.init();
-const menuBtn=document.getElementById("menu");
-const sideBar= document.getElementById("side-bar");
+
+
+const home=document.getElementById("Home");
+const menuBtn=document.getElementById("menu-bars");
+const sideBar= document.getElementById("side-menu");
 let menuStatus=false;
-let crossIcon=document.getElementById("cross-mark");
+let crossIcon=document.getElementById("cross");
 function sideMenu(){
     if(!menuStatus){
-        sideBar.style.display="flex";
+        sideBar.classList.add("show");
         menuStatus=true;
-        console.log("hello in flex");
     }
 
     else{
-        sideBar.style.display="none";       
+        
+        sideBar.classList.remove("show");  
         menuStatus=false;
-        console.log("Bye in jail");
     }
         
 }
 
+menuBtn.addEventListener("click",sideMenu);
+crossIcon.addEventListener("click",sideMenu);
 
-// menuBtn.addEventListener("click",sideMenu);
-
-// crossIcon.addEventListener("click",sideMenu);
 
 // Header hide
+const arrowUp=document.getElementById("look-up");
 const header=document.getElementById("contact-info");
 const navbar=document.getElementById("navigation-header");
 let screen = window.matchMedia("(width <= 800px)");
 let lastscrollPosition=0;
 function hideHeader(){
-
     let isScrollBottom=lastscrollPosition < window.scrollY;
     if(isScrollBottom){
         header.style.display="none";
+        // arrowUp.style.display="block";
         }
            else{
 if(screen.matches){
@@ -50,6 +62,7 @@ if(screen.matches){
 }
 else{
     header.style.display="flex";
+    // arrowUp.style.display="none";
 }        
   }
   lastscrollPosition=window.scrollY;
@@ -58,9 +71,11 @@ else{
 if(window.scrollY>=40){
     navbar.style.backgroundColor="#484444d9";
     navbar.style.paddingTop="0rem";
+  
     hideHeader();
 }
 else{
+   
     navbar.style.backgroundColor="transparent";
     if(screen.matches){
         navbar.style.paddingTop="0rem";
@@ -71,9 +86,28 @@ else{
 }
  });
 
+
+
+window.addEventListener("scroll",()=>{
+    if(window.scrollY>=1000){
+        arrowUp.style.display="block";
+    }
+else{
+    arrowUp.style.display="none";
+}
+
+
+});
+
+
+
+
+
+
+
+
+
 // Slider Image
-
-
 const previous=document.querySelector("#previous");
 const next=document.querySelector("#next");
 const sliderItems=document.querySelectorAll("#preface .preface-content");
@@ -84,7 +118,6 @@ let lastActiveImg=sliderImg[0];
 
 let updateSlider= function(){
     
-
     lastActiveSlider.classList.remove("active");
     sliderItems[position].classList.add("active");  
     lastActiveSlider=sliderItems[position];
@@ -119,10 +152,26 @@ updateSlider();
 previous.addEventListener("click",slidePrevious);
 
 let autoSlide;
-let autoSlider=function(){
-    autoSlide=setInterval(slideNext,4000);
+
+function autoSlider() {
+    autoSlide = setInterval(slideNext, 7000);
 }
 
+function handleResize() {
+    if (window.innerWidth <= 500) {
+        if (!autoSlide){
+            autoSlider();  // Start auto slider if not already running
+        }
+    } else {
+        clearInterval(autoSlide);  // Clear the interval to stop auto slider
+        autoSlide = null;  // Reset the autoSlide variable
+    }
+}
+
+// Initial check on page load
+handleResize();
+// Add event listener to handle window resize
+window.addEventListener('resize', handleResize);
 
 function analyzer(){
     if(window.innerWidth<=500){
@@ -136,13 +185,8 @@ function analyzer(){
         // clearTimeout(autoSlide);
     }
 }
-
-
+analyzer();
 window.addEventListener("resize",analyzer);
-window.addEventListener("load",analyzer);
-
-
-
 
 const imgCard = document.querySelector("#about .img-card");
 
@@ -178,22 +222,19 @@ targetBlock.addEventListener('mouseleave', () => {
     imgCard.style.transform = 'translate(0,0)';
 });
 
+});
+
+const calender=document.getElementById("calender");
+const selectedDate=document.getElementById("selected-date");
+
+calender.addEventListener("change",()=>{
+
+let dateValue=calender.value;
+selectedDate.textContent=`${dateValue}`;
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+});
 
 
 
